7d6d572eb7d2f56fb016880a5ed4c3a62427bd6d
SofiaEgo <gamova.sofi@yandex.ru>
2024-09-20 15:24:02 +0300

    Add files via upload

---
+++ precache-manifest.6bc1d135d7df53834a105fbac035f8e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a73931e2cbd420db8eee4d841cfc234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a56a3fa61cd52340ca",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "83a56a3fa61cd52340ca",
+    "url": "/static/js/main.267af2ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
